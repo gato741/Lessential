ce813bb11a8b97d5c85da1d05943a24162cec773
gato741 <maksymiliancapala1@gmail.com>
2025-02-23 14:52:06 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -7,14 +7,38 @@
 int distro;
 
 void setupArch() {
+	/* PS1 */
+	system("echo \"PS1='\\w \$ '\" >> ~/.bashrc");
+	
+	/* Packages and Updates */
+	system("pacman -Syu git base-devel flatpak gimp htop vlc wget");
+	
+	/* Paru setup */
+	system("git clone https://aur.archlinux.org/paru-bin.git");
+	system("cd paru-bin");
+	system("makepkg -si");
+	
+	/* Virtualbox setup */
+	system("pacman -S virtualbox virtualbox-host-modules-arch");
+	
+	/* Cool apps (uncomment the ones you need) */
+	// system("paru -S vesktop-bin");
+	// system("pacman -S retroarch");
+	// system("pacman -S dolphin-emu");
+	
+	system("reboot");
 }
 
 void setupDebian() {
 	/* PS1 */
 	system("echo \"PS1='\\w \$ '\" >> ~/.bashrc");
 	
+	/* Updates */
+	system("apt update");
+	system("apt upgrade");
+	
 	/* Packages */
-	system("apt install -y nala git flatpak gimp htop vlc wget");
+	system("apt install nala git flatpak gimp htop vlc wget");
 	
 	/* Virtualbox setup */
 	system("wget https://download.virtualbox.org/virtualbox/7.1.6/virtualbox-7.1_7.1.6-167084~Debian~bookworm_amd64.deb");	
@@ -24,6 +48,7 @@ void setupDebian() {
 	/* Flathub setup */
 	system("flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo");
 	/* Cool apps (uncomment the ones you need) */
+	
 	// system("flatpak install flathub dev.vencord.Vesktop");
 	// system("flatpak install flathub org.libretro.RetroArch");
 	// system("flatpak install flathub org.DolphinEmu.dolphin-emu");
